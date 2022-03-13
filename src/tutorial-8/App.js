06242e2a962bfa8562dd90411a3580068af1f31e
Artem Gurin <magickdaad@gmail.com>
2022-03-13 19:23:18 +0300

    Дз к уроку 5.4

--- src/tutorial-8/App.js
+++ src/tutorial-8/App.js
@@ -1,85 +1,89 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 import { Button, TextField } from '@mui/material';
 
-const App = () => {
-  const [filds, setFilds] = useState({ firstName: '', lastName: '', email: '', password: '' });
+function App() {
+  const { handleSubmit, register, formState, reset } = useForm();
 
-  const handleChangeInput = (evt) => {
-    setFilds({ ...filds, [evt.target.name]: evt.target.value });
-  };
-
-  const handleClickRegistr = () => {
-    if (!filds.email.includes('@')) {
-      alert('Некорректный email');
-      return;
-    }
-    if (filds.firstName.length < 3 || filds.lastName.length < 3) {
-      alert('Некорректное имя или фамилия');
-      return;
-    }
-    if (filds.password.length <= 6) {
-      alert('Некорректный пароль');
-      return;
-    }
-    console.log(filds);
-    handleClickClear();
-  };
+  const onSubmit = (values) => console.log('ФОРМА!', values);
 
   const handleClickClear = () => {
-    setFilds({ firstName: '', lastName: '', email: '', password: '' });
+    reset({
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+    });
   };
 
-  const isValid = !!filds.firstName && !!filds.lastName && !!filds.email && !!filds.password;
-
   return (
-    <div className="app">
+    <div className="App">
       <div className="row">
         <TextField
-          onChange={handleChangeInput}
-          value={filds.firstName}
           className="text"
           name="firstName"
           label="Имя"
+          {...register('firstName', {
+            validate: (value) => value !== 'admin' || 'Nice try!',
+          })}
+          helperText={formState.errors.firstName && formState.errors.firstName.message}
+          error={!!formState.errors.firstName}
         />
         <TextField
-          onChange={handleChangeInput}
-          value={filds.lastName}
           className="text"
           name="lastName"
           label="Фамилия"
+          {...register('lastName', {
+            required: 'Это обязательное поле!',
+          })}
+          helperText={formState.errors.lastName && formState.errors.lastName.message}
+          error={!!formState.errors.lastName}
         />
       </div>
       <div className="row">
         <TextField
-          onChange={handleChangeInput}
-          value={filds.email}
           className="text"
+          {...register('email', {
+            required: 'Это обязательное поле!',
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9._%+-]+\.[A-Z]{2,}$/i,
+              message: 'Это неверная почта!',
+            },
+          })}
+          helperText={formState.errors.email && formState.errors.email.message}
+          error={!!formState.errors.email}
           name="email"
           label="E-Mail"
+          defaultValue=""
         />
         <TextField
-          onChange={handleChangeInput}
-          value={filds.password}
           className="text"
+          {...register('password', {
+            required: 'Это обязательное поле!',
+          })}
+          helperText={formState.errors.password && formState.errors.password.message}
+          error={!!formState.errors.password}
           name="password"
+          type="password"
           label="Пароль"
         />
       </div>
+      <br />
       <div className="row">
         <Button
-          disabled={!isValid}
-          onClick={handleClickRegistr}
           className="btn"
+          onClick={handleSubmit(onSubmit)}
           variant="contained"
+          color="primary"
         >
           Зарегистрироваться
         </Button>
-        <Button onClick={handleClickClear} className="btn" variant="contained" color="error">
+        <Button onClick={handleClickClear} className="btn" variant="contained" color="secondary">
           Очистить
         </Button>
       </div>
     </div>
   );
-};
+}
 
 export default App;
